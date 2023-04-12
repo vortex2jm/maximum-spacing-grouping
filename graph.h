41c4486cca7d257ac260329d81f8c26077a4ad85
vortex2jm <joaopaulomoura5328@gmail.com>
2023-04-12 16:48:25 -0300

    :sparkles: Adding graph adt

--- graph.h
+++ graph.h
@@ -1,5 +1,13 @@
 #ifndef graph_h
 #define graph_h
+#include "set.h"
 
+typedef struct graph Graph;
+
+Graph * init_graph(ElementSet * set);
+
+void print_graph(Graph * graph);
+
+void end_graph(Graph * graph);
 
 #endif
