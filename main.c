3c28b1d105ffc9e6389e5d62b19ab95beaad5725
vortex2jm <joaopaulomoura5328@gmail.com>
2023-04-12 16:49:53 -0300

    :recycle: Updating main to receive graph

--- main.c
+++ main.c
@@ -1,22 +1,27 @@
-// Espaço m dimensional
-// Salvar valores das distâncias numa matriz bidimensional (pode ser um vetor)
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "element.h"
+#include "graph.h"
+#include "set.h"
 #include "util.h"
 
 int main(int argc, char const *argv[])
 {
     char *input_file = strdup(argv[1]);
     char *output_file = strdup(argv[3]);
+    int groups = atoi(argv[2]);
 
-    ElementSet *set = init_set();
-    unsigned short dimensions = read_elements(input_file, set);
+    ElementSet *set = init_set(input_file);
+    Graph *graph = init_graph(set);
+
+    print_graph(graph);
     print_set(set);
+
+    end_graph(graph);
     end_set(set);
 
     free(input_file);
     free(output_file);
+    
     return 0;
 }
