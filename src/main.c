821eaece2fc059eada5614191fe687a720705ca4
vortex2jm <joaopaulomoura5328@gmail.com>
2023-04-15 22:28:46 -0300

    :recycle: Updating main to run with new structures

--- src/main.c
+++ src/main.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include "../include/graph.h"
-#include "../include/set.h"
 #include "../include/util.h"
+#include "../include/element.h"
+#include "../include/edge.h"
 
 int main(int argc, char const *argv[])
 {
@@ -12,23 +12,28 @@ int main(int argc, char const *argv[])
     char *output_file = strdup(argv[3]);
     int groups = atoi(argv[2]);
 
-    ElementSet *set = init_set(input_file);
-    Graph *graph = init_graph(set);
+    FILE * file = fopen(input_file,"r");
+    int element_set_size = get_lines_amount(file);
+    int dimension = get_dimension(file);
+    int graph_size = calculate_graph_size(element_set_size);
+
+    Element ** elements = read_elements(file, element_set_size, dimension);
+    Edge ** graph = generate_graph(elements, graph_size, element_set_size, dimension);    
+
+    printf("dimension = %d\n", dimension);
+    printf("graph size = %d\n", graph_size);
+
+    qsort(graph, graph_size, sizeof(Edge*), edge_comparator);
+    print_graph(graph, graph_size);
+    
+    // print_elements(elements, element_set_size, dimension);
 
     // clock_t sort_time_start, sort_time_end;
-    // sort_time_start = clock();
-    
-    sort_graph(graph);
-    
+    // sort_time_start = clock();    
     // sort_time_end = clock();
 
     // double sort_time_seconds = ((double)sort_time_end - sort_time_start)/CLOCKS_PER_SEC;
     // printf("sort time = %lf\n", sort_time_seconds);
-    // print_graph(graph);
-    // print_set(set);
-
-    end_graph(graph);
-    end_set(set);
 
     free(input_file);
     free(output_file);
